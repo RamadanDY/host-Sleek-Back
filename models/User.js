497002ec5c5eb372fd9a9637de7e9430c6d0a512
RamDev <daudalaramadan@gmail.com>
2025-09-03 02:49:09 +0000

    Make phoneNumber optional in User schema

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  phoneNumber: { type: String, required: false, unique: true }, // Added for phone-based login
+  // phoneNumber: { type: String, required: false, unique: true }, // Added for phone-based login
   password: { type: String, required: true },
 });
 
